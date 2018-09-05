03af4cbf84dafb3369608a10a2d6274c778d1d1c
Valentin Dumas <valentin.dumas@viacesi.fr>
2018-09-05 23:31:28 +0200

    Include clean ImGui (GL3/GLFW with GLEW loader)

--- main.cpp
+++ main.cpp
@@ -5,15 +5,37 @@
 #include <iostream>
 #include "include/shader.h"
 #include "include/ResourceManager.h"
-#include <glm/gtc/matrix_transform.inl>
+#include <glm/gtc/matrix_transform.hpp>
+
+///////////////// IMGUI /////////////////
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_glfw.h"
+#include "ImGui/imgui_impl_opengl3.h"
+#pragma region Headers included, based on IMGUI_IMPL_OPENGL_LOADER_... directive
+//#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+//#include <GL/gl3w.h>    // Initialize with gl3wInit()
+//#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+//#include <GL/glew.h>    // Initialize with glewInit()
+//#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+//#include <glad/glad.h>  // Initialize with gladLoadGL()
+//#else
+//#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+//#endif
+#pragma endregion
+/////////////////////////////////////////
+
+void glfw_error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
 
 // GLFW function declerations
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 // The Width of the screen
-const GLint SCREEN_WIDTH = 800;
+const GLint SCREEN_WIDTH = 1080;
 // The height of the screen
-const GLint SCREEN_HEIGHT = 600;
+const GLint SCREEN_HEIGHT = 720;
 
 //DreamInEngine(SCREEN_WIDTH, SCREEN_HEIGHT);
 
@@ -28,20 +50,36 @@ enum ProjectionType
 
 int main(int argc, char *argv[])
 {
-	ProjectionType projection_type = PROJECTION_ORIGIN_CENTER;
+	ProjectionType projection_type = PROJECTION_NORMALIZED_ORIGIN_CENTER;
 
-	glfwInit();
+	glfwSetErrorCallback(glfw_error_callback);
+	if (!glfwInit()) return 1;
+
+#if __APPLE__
+	const char* glsl_version = "#version 150";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required on Mac
+#else
+	const char* glsl_version = "#version 130";
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif	
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "FuryRunner", (fullscreen ? glfwGetPrimaryMonitor() : nullptr), nullptr);
+	if (!window) return 1;
 	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 
 	//glewExperimental = GL_TRUE;
-	glewInit();
-	glGetError(); // Call it once to catch glewInit() bug, all other errors are now from our application.
+	if (glewInit() != GLEW_OK)
+	{
+		fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+		return 1;
+	}
 
 	glfwSetKeyCallback(window, key_callback);
 
@@ -82,8 +120,91 @@ int main(int argc, char *argv[])
 	ResourceManager::LoadShader("resources/shaders/square.vert", "resources/shaders/square.frag", nullptr, "square_shader");
 	ResourceManager::GetShader("square_shader").Use();
 
+#pragma region ImGui (setup)
+	///////////// IMGUI (SETUP) /////////////
+	// Setup Dear ImGui binding
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	// Setup style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// Load Fonts
+	// - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+	// - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+	// - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+	// - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+	// - Read 'misc/fonts/README.txt' for more instructions and details.
+	// - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+	//io.Fonts->AddFontDefault();
+	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+	//ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+	//IM_ASSERT(font != NULL);
+
+	bool show_demo_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+#pragma endregion 
+
 	while (!glfwWindowShouldClose(window))
 	{
+		// Catch Queued Events
+		glfwPollEvents();
+
+		// Start the Dear ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+#pragma region ImGui Window creation
+		// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
+
+		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+		{
+			static float f = 0.0f;
+			static int counter = 0;
+
+			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+			ImGui::Checkbox("Another Window", &show_another_window);
+
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+			if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+				counter++;
+			ImGui::SameLine();
+			ImGui::Text("counter = %d", counter);
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			ImGui::End();
+		}
+
+		// 3. Show another simple window.
+		if (show_another_window)
+		{
+			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+			ImGui::Text("Hello from another window!");
+			if (ImGui::Button("Close Me"))
+				show_another_window = false;
+			ImGui::End();
+		}
+#pragma endregion
+
 		// Calculate delta time
 		GLfloat currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
@@ -97,39 +218,32 @@ int main(int argc, char *argv[])
 		//	startFrame += 1.0;
 		//}
 
-		//deltaTime = 0.001f;
-		// Manage user input
+		// Manage user input(s)
 
 		// Update Game state
 
-		// Clear Screen before render
-		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT/* | GLFW_DOUBLEBUFFER*/);
-
 		glm::mat4 projection(1.0f);
-		
-		if(PROJECTION_ORIGIN_CENTER)
+		switch (projection_type)
 		{
-			
-		}
-		if(PROJECTION_NORMALIZED_ORIGIN_CENTER)
-		{
+		case PROJECTION_NORMALIZED_ORIGIN_CENTER:
 			projection = glm::ortho(static_cast<GLfloat>(SCREEN_WIDTH*(-1) / 2), static_cast<GLfloat>(SCREEN_WIDTH / 2), static_cast<GLfloat>(SCREEN_HEIGHT*(-1) / 2), static_cast<GLfloat>(SCREEN_HEIGHT / 2), -1.0f, 1.0f);
+			ResourceManager::GetShader("square_shader").SetMatrix4("projection", projection);
+			break;
+		case PROJECTION_NORMALIZED_ORIGIN_TOP_LEFT:
+			projection = glm::ortho(0.0f, static_cast<GLfloat>(SCREEN_WIDTH), static_cast<GLfloat>(SCREEN_HEIGHT), 0.0f, -1.0f, 1.0f);
+			ResourceManager::GetShader("square_shader").SetMatrix4("projection", projection);
+			break;
+		default:
+			break;
 		}
-		if(PROJECTION_NORMALIZED_ORIGIN_TOP_LEFT)
-		{
-			
-		}
-		ResourceManager::GetShader("square_shader").SetMatrix4("projection", projection);
 
-		// Render
+		// Rendering GL entities
 		glm::mat4 model(1.0f);					// POSITION
 		model = glm::translate(model, glm::vec3(-310.0f, -150, 0.0f));
 		model = glm::scale(model, glm::vec3(100.0f, 100.0f, 1.0f));
 		ResourceManager::GetShader("square_shader").SetMatrix4("model", model);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
-
 		model = glm::mat4(1.0f);               // x  // y  // z
 		model = glm::translate(model, glm::vec3(50.0f, 200.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(100.0f, 100.0f, 1.0f));
@@ -137,16 +251,27 @@ int main(int argc, char *argv[])
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 
-		// glfwSwapInterval(1);
-		glfwSwapBuffers(window);
+		ImGui::Render();
 
-		// get events
-		glfwPollEvents();
+		// Clear ?
+		int display_w, display_h;
+		glfwMakeContextCurrent(window);
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT/* | GLFW_DOUBLEBUFFER*/);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwMakeContextCurrent(window);
+		glfwSwapBuffers(window);
 	}
 
-	// Delete all resources as loaded using the resource manager
-	// DreamInEngine.Close();
+	// ImGui cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
+	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
